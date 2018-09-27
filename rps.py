7c983c99577fc9a1acc1d738c1855b1434dfbabf
Sean von Bayern <seanvonbayern@gmail.com>
2018-09-26 18:49:32 -0700

    Add HumanPlayer behavior and input validation

--- rps.py
+++ rps.py
@@ -4,7 +4,7 @@
 import random
 
 
-moves = ['rock', 'paper', 'scissors']
+moves = ["Rock", "Paper", "Scissors"]
 
 
 class Player:
@@ -12,7 +12,7 @@ class Player:
         self.score = 0
     
     def move(self):
-        return 'rock'
+        return "Rock"
 
     def learn(self, my_move, their_move):
         pass
@@ -22,8 +22,31 @@ class HumanPlayer(Player):
     def __init__(self):
         super().__init__()
         self.name = "You"
+    
+    def move(self):
+        choice = 0
+        fails = 0
+        while choice == 0:
+            if fails == 0:
+                move_choice = str(input("\nWhat will you throw: Rock, Paper, or Scissors? "))
+            else:
+                move_choice = str(input("\nTry again: Rock, Paper, or Scissors? "))
+            if move_choice[0].upper() == "R":
+                choice = "Rock"
+            elif move_choice[0].upper() == "P":
+                choice = "Paper"
+            elif move_choice[0].upper() == "S":
+                choice = "Scissors"
+            else:
+                if fails <= 2:
+                    fails += 1
+                    print(f"\n'Flag on the play! The referee rules that '{move_choice}' isn't a tournament-legal throw!'")
+                else:
+                    choice = random.choice(moves)
+                    print(f"\n'The referee rules that the garbage you're throwing most closely resembles {choice}!'")
+        return choice
 
-   
+
 class RockPlayer(Player):
     def __init__(self):
         super().__init__()
@@ -52,9 +75,9 @@ class CyclePlayer(Player):
 
 
 def beats(one, two):
-    return ((one == 'rock' and two == 'scissors') or
-            (one == 'scissors' and two == 'paper') or
-            (one == 'paper' and two == 'rock'))
+    return ((one == "Rock" and two == "Scissors") or
+            (one == "Scissors" and two == "Paper") or
+            (one == "Paper" and two == "Rock"))
 
 
 class Game:
@@ -65,13 +88,13 @@ class Game:
     def play_round(self):
         move1 = self.p1.move()
         move2 = self.p2.move()
-        print(f"Player 1: {move1}  Player 2: {move2}")
+        print(f"\nPlayer 1: {move1}  Player 2: {move2}")
         if beats(move1, move2):
             self.p1.score += 1
-            print(f"That's a point for {self.p1.name}!""\n")
+            print(f"That's a point for {self.p1.name}!")
         elif beats(move2, move1):
             self.p2.score += 1
-            print(f"That's a point for {self.p2.name}!""\n")
+            print(f"That's a point for {self.p2.name}!")
         else:
             print("It's a draw!""\n")
         self.p1.learn(move1, move2)
@@ -79,17 +102,17 @@ class Game:
 
     def play_game(self):
         round = 0
-        print("Game start!")
+        print("\nGame start!")
         while self.p1.score <= 1 and self.p2.score <= 1:
             round += 1
-            print(f"Round {round}:")
+            print(f"\nRound {round}:")
             self.play_round()
         if self.p1.score == 2:
-            print(f"{self.p1.name} win! Congratulations!")
+            print(f"\n{self.p1.name} win! Congratulations!")
         else:
-            print(f"{self.p2.name} wins! Better luck next time!")
+            print(f"\n{self.p2.name} wins! Better luck next time!")
 
 
 if __name__ == '__main__':
-    game = Game(RockPlayer(), RandomPlayer())
+    game = Game(HumanPlayer(), RandomPlayer())
     game.play_game()
